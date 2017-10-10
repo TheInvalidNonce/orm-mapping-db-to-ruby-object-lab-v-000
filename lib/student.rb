8cfd0b1293fe466e27a25d8933617feeedba2e97
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-10 15:29:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,9 @@ class Student
 
   def self.new_from_db(row)
     sql = "SELECT row from students"
+    binding.pry
 
-    
+
   end
 
   def self.all
@@ -18,16 +19,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
