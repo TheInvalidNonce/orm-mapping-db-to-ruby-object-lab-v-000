2c99a1118a38398afe78878e77132693c8cb9bf7
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-10 15:53:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    sql = "SELECT * FROM students WHERE grade = 10 ORDER BY name LIMIT 1"
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
 
     DB[:conn].execute(sql)
   end
