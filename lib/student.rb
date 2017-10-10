4b2ed397395acd12e1a39cb63bca0acfa7728dc8
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-10 15:50:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,11 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+
+    DB[:conn].execute(sql, x)
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
